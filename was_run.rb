6f7bead3c720a0d44787f59079c211485cd756e6
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-03-01 03:05:13 -0500

    Use Pluggable Selector to invoke dynamically @name attribute string content "test_method"
    Refactor: take code that works in one instance and generalize it
    to work in many by replacing constants with variables.

--- was_run.rb
+++ was_run.rb
@@ -2,11 +2,12 @@ class WasRun
   attr_reader :was_run
   def initialize name
     @was_run = nil
+    @name = name
   end
   def test_method
     @was_run = 1
   end
   def run
-    test_method
+    method(@name).call
   end
 end
